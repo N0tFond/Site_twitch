a52e3a9a11507512a079208ef3e1782b0ad93843
Not <92156365+N0tFond@users.noreply.github.com>
2024-02-21 23:28:38 +0100

    refact buttons

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-const button = document.getElementById('twitch') as HTMLButtonElement;
-button.onclick = function() {
-    window.open('https://www.twitch.tv/not_fond', '_blank')
-}
+const openLink = () => {
+ window.open("https://www.twitch.tv/not_fond", "_blank")
+};
